ed4fbf599030e07a32b92666abd52383b200e136
niccokuc <nerminkuc74@gmail.com>
2016-01-18 19:42:03 +0400

    push back

---
+++ joins/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Join(models.Model):
+    name = models.TextField()
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add= True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add= False, auto_now=True)
+
+    def __str__(self):
+        return "Helloworld"
